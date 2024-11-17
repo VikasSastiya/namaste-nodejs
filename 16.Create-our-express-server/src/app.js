4b81ac3215bc4c60fc2d9b0e7048bad44ace79c3
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-11-17 17:01:43 +0530

    commit

--- 16.Create-our-express-server/src/app.js
+++ 16.Create-our-express-server/src/app.js
@@ -1,1 +1,22 @@
-gbg
+console.log("Starting a new project!");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use("/",(req,res)=>{
+      res.send("Namaste from the Dashboard");
+      
+});
+
+app.use('/hello', (req, res) => {
+  res.send('Hello hello hello!')
+});
+
+app.use("/test",(req,res)=>{
+  res.send("Hello from the server!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
