af30c95d32d152cb1ef228b0b8fbb4b351939ff9
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2025-01-07 16:09:05 +0530

    Exploring GET,POST and Delete

--- S02E04RoutingAndRequestHandlers/src/app.js
+++ S02E04RoutingAndRequestHandlers/src/app.js
@@ -4,19 +4,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use("/vikas/sir",(req,res)=>{
-       res.send("hi vikas sastiya");
-});
-app.use("/vikas",(req,res)=>{
-     res.send("hi sastiya"); 
+app.use("/vikas",(req,res)=>{     // just shift the code from upper to lower your problem which is hi vikasshowing in every route get solved so from above discussion we learn order of route or any code isvery very important
+    res.send("Hello !");
 });
 
-app.use("/sir",(req,res)=>{
-       res.send("hi sir");
+app.get("/vikas",(req,res)=>{
+     res.send({name:"vikas",status:"bhakti"}); 
 });
-app.use("/",(req,res)=>{     // just shift the code from upper to lower your problem which is hi vikasshowing in every route get solved so from above discussion we learn order of route or any code isvery very important
-    res.send("hi vikas");
+
+app.post("/vikas",(req,res)=>{
+       res.send("Data successfully saved to the database");
 });
-app.listen(1000,()=>{
-  console.log("server is listening at 1000");
+app.use("/test",(req,res)=>{     
+    res.send("Hello from the server!");
+});
+app.delete("/vikas",(req,res)=>{
+     res.send("Deleted successfuly forom the server");  
+})
+app.listen(port,()=>{
+  console.log("server is listening at 3000");
 });
